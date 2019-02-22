a971b40cd83550d2a71c201cb252eac406d9ba8e
Kenneth Nicholas <nicholas.kenneth.j@gmail.com>
2019-02-22 15:33:56 -0600

    react-clock

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function tick() {
+    const element = (
+        <div>
+            <h1>My First React Lab</h1>
+            <h2>The time is <span>{new Date().toLocaleTimeString()}.</span></h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
